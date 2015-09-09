b0028001c8f40ea25fa060151bf5e110437d270f
Nguyen Hoang Long <roman.long@email.cz>
2015-09-09 15:27:48 +0200

    refactoring

--- scripts/concept_linking/concept_linking_performance.py
+++ scripts/concept_linking/concept_linking_performance.py
@@ -2,7 +2,7 @@
 # Measures YodaQA concept linking performance
 # requires the fixed QuestionDump json and the dataset
 # exact match means all correct concepts were found
-# recall means -some- (atleast one) correct concept were found
+# partial match means -some- (atleast one) correct concept were found
 # incorrect means no expected concept was found
 # then there is the total number of concepts found vs expected
 # usage:
@@ -14,8 +14,8 @@ import itertools
 
 
 def compare(dataset, correct):
-    recall = 0
-    correct_ans = 0
+    partial_match = 0
+    exact_match = 0
     concepts_to_be_found = 0
     concepts_found = 0
     total = len(dataset)
@@ -40,14 +40,14 @@ def compare(dataset, correct):
             continue
         if found_all is True:
             print("full match for " + str(entry['qId']))
-            correct_ans += 1
+            exact_match += 1
         else:
-            print("recall for " + str(entry['qId']))
-            recall += 1
-    print("exact match: {0} questions".format(correct_ans))
-    print("recall: {0} questions".format(recall))
-    print("not found: {0} questions".format(total - (recall + correct_ans)))
-    print("recall or exact match: {0}% questions".format((correct_ans + recall) / float(total) * 100))
+            print("partial_match for " + str(entry['qId']))
+            partial_match += 1
+    print("exact match: {0} questions".format(exact_match))
+    print("partial_match: {0} questions".format(partial_match))
+    print("not found: {0} questions".format(total - (partial_match + exact_match)))
+    print("partial or exact match: {0}% questions".format((exact_match + partial_match) / float(total) * 100))
     print("found {0} out of {1} concepts, {2}% ".format
           (concepts_found, concepts_to_be_found,
            (concepts_found / float(concepts_to_be_found)) * 100))
